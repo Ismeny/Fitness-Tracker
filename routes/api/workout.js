bca3dd866f4348334bdc7484c17b22816895e2c5
Ismeny Saguilan <menysag2@gmail.com>
2021-11-18 21:14:51 -0600

    workout js routes

--- routes/api/workout.js
+++ routes/api/workout.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const { Workout } = require ("../../models")
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 //aggregate functions
 router.get ("/workouts", async (req, res) => {
